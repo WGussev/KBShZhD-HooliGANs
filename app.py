7ba24635f243e6181501790db9819d9c925eebc2
WGussev <wsewolodgusew@yandex.ru>
2020-11-29 14:39:55 +0300

    gather project files

---
+++ app.py
@@ -0,0 +1,128 @@
+import base64
+import io
+import pathlib
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+from dash.dependencies import Input, Output, State, ClientsideFunction
+from plotly.express import bar
+import plotly.express as px
+
+# get relative data folder
+PATH = pathlib.Path(__file__).parent
+DATA_PATH = PATH.joinpath("data").resolve()
+
+app = dash.Dash(__name__,
+                meta_tags=[{"name": "viewport", "content": "width=device-width"}])
+server = app.server
+
+
+df_consumption = pd.read_csv('/home/gustav/Projects/KBShZhD/data/consumption.csv')
+
+
+# LAYOUT
+app.layout = html.Div(
+    [html.Div(id="output-clientside"), # empty Div to trigger javascript file for graph resizing
+    html.H2("Прогноз расхода для плана работ"),
+    html.Div(
+        [html.Div(
+            [html.Div(className='pretty_container',
+                      children=html.Div(
+                               dcc.Upload(id='upload-data',
+                                          children=html.Div(['Перетащите сюда или ',
+                                                             html.A('загрузите файлы')])))),
+            html.Div(
+                    dcc.Dropdown(id='agg-dropdown',
+                        options=[{'label': 'Вид работ', 'value': 'work'},
+                                 {'label': 'Машина', 'value': 'machine'}],
+                        value='work'),
+                    className='pretty_container',),
+            html.Div(
+                dcc.Dropdown(id='precise-dropdown',
+                             options=[{'label': 'все', 'value': 'all'}],
+                             value='all'), className='pretty_container'),
+            html.Center([
+                html.H2('общий расход'),
+                html.H2('-----', id='total-label', style={'font-weight': 'bold'}),
+                html.H2('литров')],
+                className='pretty_container')],
+            className="three columns",
+            id="cross-filter-options",
+            ),
+            html.Div(
+                html.Div(
+                    dcc.Graph(id="count_graph"),
+                    id="countGraphContainer",
+                    className="pretty_container",
+                    style={'height': '600px'}),
+                id="right-column",
+                className="nine columns")],
+        className="row flex-display",
+    ),
+    html.H2("Статистика расхода топлива")],
+    id="mainContainer",
+    style={"display": "flex", "flex-direction": "column"})
+
+# CALLBACKS
+app.clientside_callback(
+    ClientsideFunction(namespace="clientside", function_name="resize"),
+    Output("output-clientside", "children"),
+    [Input("count_graph", "figure")],
+)
+
+
+@app.callback([Output('total-label', 'children'),
+              Output('count_graph', 'figure'),
+               Output('precise-dropdown', 'options')],
+              [Input('upload-data', 'contents'),
+               Input('agg-dropdown', 'value'),
+               Input('precise-dropdown', 'value')],
+              [State('upload-data', 'filename'),
+              State('upload-data', 'last_modified')])
+def update_output(list_of_contents, agg_col, second_level, list_of_names, list_of_dates):
+
+    if list_of_contents is None:
+        return '-----', bar(), []
+
+    content_type, content_string = list_of_contents.split(',')
+    decoded = base64.b64decode(content_string)
+
+    try:
+        df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+    except Exception as e:
+        print(e)
+        return html.Div(['There was an error processing this file.'])
+
+    df_calc = pd.merge(df_consumption, df, on=['machine', 'work'], how='inner').reset_index()
+
+    second_level_values = [{'label': 'все', 'value': 'all'}, ] + [{'label': item, 'value': item} for item in df_calc.reset_index()[agg_col].unique()]
+
+    if second_level == 'all':
+        df_total = df_calc.groupby(agg_col).sum().reset_index().sort_values(agg_col)
+        fig = bar(df_total, y=agg_col, x="volume",
+                  labels={"volume": "Расход топлива (л)",
+                          agg_col: "Машина/работа"},
+                  title="Расход топлива", orientation='h',
+                  color_continuous_scale='reds',
+                  color="volume")
+        consumption = int(df_calc['volume'].sum())
+    else:
+        df_subtotal = df_calc[df_calc[agg_col] == second_level].sort_values(agg_col)
+        fig = bar(df_subtotal, y=agg_col, x="volume",
+                  labels={"volume": "Расход топлива (л)",
+                          agg_col: "Машина/работа"},
+                  title="Расход топлива", orientation='h',
+                  color_continuous_scale='reds',
+                  color="volume")
+        consumption = int(df_subtotal['volume'].sum())
+
+    fig.update_layout(barmode='stack', yaxis={'categoryorder': 'total ascending'})
+    return str(consumption), fig, second_level_values
+
+
+# Main
+if __name__ == "__main__":
+    app.run_server(port=8877, debug=True)
