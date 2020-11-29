a36049d3e7ea66cde7c459d822d456c22596bd76
WGussev <wsewolodgusew@yandex.ru>
2020-11-29 15:11:26 +0300

    add forgotten file

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import pandas as pd
 from dash.dependencies import Input, Output, State, ClientsideFunction
 from plotly.express import bar
 import plotly.express as px
+import dash_table
+import numpy as np
+
 
 # get relative data folder
 PATH = pathlib.Path(__file__).parent
@@ -21,6 +24,85 @@ server = app.server
 
 
 df_consumption = pd.read_csv('/home/gustav/Projects/KBShZhD/data/consumption.csv')
+
+
+FILENAME = "/home/gustav/Projects/KBShZhD/data/raw.xlsx"
+df = pd.read_excel(FILENAME)
+df['Date'] = pd.to_datetime(df['Date'])
+
+
+def get_bootstrap_rates(df, col_name, n_iterations=100):
+    df = df[(df[col_name] <= np.quantile(df[col_name], 0.95)) & (df[col_name] >= np.quantile(df[col_name], 0.05))]
+    n_samples = df.shape[0]
+    means = []
+    stdevs = []
+    for i in range(n_iterations):
+        samples = np.random.randint(0, n_samples, size=n_samples)
+        temp = df.iloc[samples]
+        means.append(temp[col_name].mean())
+        stdevs.append(temp[col_name].std())
+    means = np.asarray(means)
+    stdevs = np.asarray(stdevs)
+    result = {
+        'Тип машины': df.index.unique()[0][0],
+        'Тип работы': df.index.unique()[0][1],
+        'Нижняя граница доверительного интервала среднего': np.quantile(means, 0.025),
+        'Верхняя граница доверительного интервала среднего': np.quantile(means, 0.975),
+        'Нижняя граница доверительного интервала стандартного отклонения': np.quantile(stdevs, 0.075),
+        'Верхняя граница доверительного интервала стандартного отклонения': np.quantile(stdevs, 0.975)
+    }
+    return result
+
+
+def generate_table(dataframe, max_rows=30):
+    df_stats = dataframe.groupby(['Тип машины', 'Тип работы']).agg(
+        {'Среднее потребление на работу': [np.mean, np.std, np.count_nonzero]})
+    df_stats.columns = ['Среднее потребление топлива на работу', 'Стандартное отклонение потребления топлива на работу',
+                        'Количество работ']
+    df_index_bs = df_stats[df_stats['Количество работ'] >= 10].index
+    dataframe = dataframe.set_index(['Тип машины', 'Тип работы'])
+    df_bs = dataframe[dataframe.index.isin(df_index_bs)]
+    stats_list = []
+    for index in df_bs.index.unique():
+        res = get_bootstrap_rates(df_bs.loc[index], col_name='Среднее потребление на работу')
+        stats_list.append(res)
+
+    add_stats = pd.DataFrame(stats_list)
+    add_stats = add_stats.set_index(['Тип машины', 'Тип работы'])
+    df_final_stats = pd.merge(df_stats, add_stats, how='left', left_index=True, right_index=True).iloc[:, :5]\
+        .round(decimals=1)
+    df_final_stats = df_final_stats.reset_index().fillna('Недостаточно данных')\
+        .sort_values(by='Среднее потребление топлива на работу', ascending=False)
+    return dash_table.DataTable(
+        id='table',
+        style_data={
+            'whiteSpace': 'normal',
+            'height': 'auto',
+            'width': 'auto'
+        },
+        columns=[{'name': col, 'id': col} for col in df_final_stats.columns],
+        data=df_final_stats.to_dict('records'),
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        column_selectable="single",
+        row_selectable="multi",
+        row_deletable=True,
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current=0,
+        page_size=10
+    )
+
+
+dates = df['Date'].unique()
+dates.sort()
+dates_dict = {}
+i = 1
+for date in dates:
+    dates_dict[i] = date
+    i += 1
 
 
 # LAYOUT
@@ -62,9 +144,19 @@ app.layout = html.Div(
                 className="nine columns")],
         className="row flex-display",
     ),
-    html.H2("Статистика расхода топлива")],
-    id="mainContainer",
-    style={"display": "flex", "flex-direction": "column"})
+    html.H2("Статистика расхода топлива"),
+    dcc.RangeSlider(id='date-slider',
+        min=1,
+        max=i,
+        value=[1, i],
+        step=1,
+        marks={
+            1: {'label': '01-10-2019'},
+            i: {'label': '31-07-2020'}
+        }),
+    html.H5(children='     '),
+    generate_table(df)
+])
 
 # CALLBACKS
 app.clientside_callback(
